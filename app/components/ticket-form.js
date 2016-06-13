ba6067d69272fb8c4717b43c1628a5febb7948f5
ming wen and nick arrasate <mwen82@gmail.com>
2016-06-13 10:31:25 -0700

    Spruce up styling

--- app/components/ticket-form.js
+++ app/components/ticket-form.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
-  
+
   actions: {
     submitTicket(){
       var params = {
         name: this.get('name'),
         location: this.get('location'),
-        issue: this.get('issue')
+        issue: this.get('issue'),
+        moment: moment().format('h:mm a')
       };
       this.sendAction('submitTicket', params);
       this.set('name', '');
--- app/models/ticket.js
+++ app/models/ticket.js
@@ -1,9 +1,8 @@
 import DS from 'ember-data';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: DS.attr(),
   location: DS.attr(),
-  issue: DS.attr()
+  issue: DS.attr(),
+  moment: DS.attr()
 });
